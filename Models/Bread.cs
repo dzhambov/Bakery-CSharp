8c3b00c3222d28f52611cbb27d7ce141b73a5c0d
Hristo Dzhambov <dzhambov@hotmail.com>
2020-03-07 13:36:48 -0800

    add changes to the bread business logic and creat pastry.cs file.

---
+++ Models/Bread.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Bakery.Models 
+{
+
+  public class Bread
+  {
+    public string LoafKind { get; set; }
+    public int LoafNum { get; set; }
+    public int LoafPrice { get; set; }
+    
+  public BreadOrder ( string LoafKind, int LoafNum, int LoafPrice )
+    {
+      LoafKind = loafKind;
+      LoafNum = loafNum;
+      LoafPrice = loafPrice;
+    }
+    
+  }
+}
+
+
+
+
